decb5ebe31abdea12d32b796dcb4bc44c60b1671
Vlad <kzvlad3@gmail.com>
2021-10-15 14:15:08 +0700

    partners href

--- src/components/sections/Partners/Partners.tsx
+++ src/components/sections/Partners/Partners.tsx
@@ -4,8 +4,8 @@ import styles from './styles.module.css'
 import { HeaderLink } from "common/constants/HeaderLinks";
 
 const data = [
-  {src: '/partners/partner1.svg'},
-  {src: '/partners/partner2.svg'},
+  {src: '/partners/partner1.svg', url: 'https://daomaker.com'},
+  {src: '/partners/partner2.svg', url: 'https://infinitypad.com'},
 ];
 
 const Partners = () => {
@@ -14,9 +14,9 @@ const Partners = () => {
       <Text type="h2" className="w-full justify-center text-center mb-10">Partners</Text>
       <div className="flex w-full justify-center flex-col items-center lg:justify-start md:flex-row">
         {data.map((item, i) => (
-          <div key={i} className={styles.root + " flex flex-col px-4 py-4 w-80 md:w-64 md:w-72 mb-10" + (i === 0 ? ' md:mr-12' : '')}>
+          <a href={item.url} key={i} target="_blank" className={styles.root + " flex flex-col px-4 py-4 w-80 md:w-64 md:w-72 mb-10" + (i === 0 ? ' md:mr-12' : '')}>
             <img src={item.src} />
-          </div>
+          </a>
         ))}
       </div>
     </div>
