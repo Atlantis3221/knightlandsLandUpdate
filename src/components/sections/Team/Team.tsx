b3a5418af9446fdd518b0f97f26ade07a6e47bca
Vlad <kzvlad3@gmail.com>
2021-10-17 12:36:33 +0700

    update team; fix token-mobile.png

--- src/components/sections/Team/Team.tsx
+++ src/components/sections/Team/Team.tsx
@@ -5,15 +5,18 @@ import { HeaderLink } from "common/constants/HeaderLinks";
 const data = [
   {src: '/team/team1.png', name: 'Denis Ermolin', description: '10 years of experience. Animoca Brands. Coins & Steel. Polygon Technology'},
   {src: '/team/team2.png', name: 'Andrey Pavlychev', description: '14 years of experience. Gameloft. Coins & Steel'},
+  {src: '/team/team3.png', name: 'Denis Savin', description: ''},
+  {src: '/team/team4.png', name: 'Matvei', description: ''},
+  {src: '/team/team5.png', name: 'Yan Sean', description: ''},
 ];
 
 const Team = () => {
   return (
-    <div className="flex flex-col justify-start w-full px-12" id={HeaderLink.TEAM}>
+    <div className="flex flex-col justify-start w-full py-36" id={HeaderLink.TEAM}>
       <Text type="h2" className="w-full justify-center text-center mb-10">Team</Text>
-      <div className="flex w-full justify-center flex-col items-center lg:justify-start md:flex-row">
+      <div className="flex w-full justify-center sm:flex-wrap md:justify-start flex-col items-center sm:flex-row">
         {data.map((item, i) => (
-          <div key={i} className={"flex flex-col items-center text-center w-64 md:w-72 mb-10 md:mr-4" + (i === 0 ? ' md:mr-4' : '')}>
+          <div key={i} className="flex flex-col w-80 md:w-52 items-center text-center mb-10 sm:mx-2">
             <img src={item.src} className="mb-6" />
             <Text type="h4" className="mb-2 uppercase">{item.name}</Text>
             <Text type="h5" className="h-20">{item.description}</Text>
