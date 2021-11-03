8838990f7c1c7f11f466be71235d3201a5e03c8b
circuspony <endofrever@gmail.com>
2021-11-03 21:10:00 +0700

    fix: remove padding in features

--- src/components/sections/MainInfo/Features/Features.tsx
+++ src/components/sections/MainInfo/Features/Features.tsx
@@ -5,7 +5,7 @@ import Text from "components/common/Text/Text";
 
 const Features = () => {
   return (
-    <div data-aos="fade-up" data-aos-delay={500} className="grid grid-cols-1 lg:grid-cols-3 w-full mb-8 gap-x-4 gap-y-4 w-full px-4">
+    <div data-aos="fade-up" data-aos-delay={500} className="grid grid-cols-1 lg:grid-cols-3 w-full mb-8 gap-x-4 gap-y-8 w-full">
       <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative"}>
         <div className="absolute bottom-0 -right-4 w-56 sm:w-1/2 lg:w-full my-auto lg:my-0 h-32 sm:relative">
           <img src="/main/map.png" className="absolute bottom-0 w-full xl:w-96" />
