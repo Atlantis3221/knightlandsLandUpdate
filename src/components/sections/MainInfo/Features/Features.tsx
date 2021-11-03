e42d47da28f02ed4c7a613ec89ac5e1a9db26f04
circuspony <endofrever@gmail.com>
2021-11-03 21:37:36 +0700

    fix: fix feature mobile overflow issue

--- src/components/sections/MainInfo/Features/Features.tsx
+++ src/components/sections/MainInfo/Features/Features.tsx
@@ -5,8 +5,8 @@ import Text from "components/common/Text/Text";
 
 const Features = () => {
   return (
-    <div data-aos="fade-up" data-aos-delay={500} className="grid grid-cols-1 lg:grid-cols-3 w-full mb-8 gap-x-4 gap-y-8 w-full">
-      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative"}>
+    <div data-aos="fade-up" data-aos-delay={500} className="grid grid-cols-1 lg:grid-cols-3 w-full mb-8 gap-x-4 gap-y-8 w-full ">
+      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative overflow-hidden sm:overflow-visible "}>
         <div className="absolute bottom-0 -right-4 w-56 sm:w-1/2 lg:w-full my-auto lg:my-0 h-32 sm:relative">
           <img src="/main/map.png" className="absolute bottom-0 w-full xl:w-96" />
         </div>
@@ -17,7 +17,7 @@ const Features = () => {
           </Text>
         </div>
       </div>
-      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative"}>
+      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative overflow-hidden sm:overflow-visible "}>
         <div className="flex justify-center absolute bottom-0 right-0 w-56 sm:w-1/2 lg:w-full my-auto lg:my-0 h-32 sm:relative ">
           <img src="/main/battle.png" className="absolute bottom-0 w-full xl:w-80" />
         </div>
@@ -28,7 +28,7 @@ const Features = () => {
           </Text>
         </div>
       </div>
-      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative"}>
+      <div className={styles.feature + " h-full py-8 flex flex-row-reverse lg:flex-col relative overflow-hidden sm:overflow-visible "}>
         <div className="absolute bottom-0 -right-4 w-56 sm:w-1/2 lg:w-full my-auto lg:my-0 h-32 sm:relative">
           <img src="/main/map.png" className="absolute bottom-0 w-full xl:w-96" />
         </div>
