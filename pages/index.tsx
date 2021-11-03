7126222e784602e7eeec08c8f2a2478a2a0085d6
circuspony <endofrever@gmail.com>
2021-11-03 21:10:19 +0700

    feat: sequel section

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,33 +12,38 @@ import Token from "components/sections/Token/Token";
 import Layout from "components/Layout";
 import Team from "components/sections/Team/Team";
 import Partners from "components/sections/Partners/Partners";
+import Sequel from "components/sections/Sequel/Sequel";
 
 export default function Home() {
   return (
     <Layout>
       <div className="w-full h-full">
-        <Header/>
+        <Header />
 
         <Container>
-          <MainInfo/>
+          <MainInfo />
         </Container>
 
-        <Army/>
+        <Army />
 
         <Container disableVerticalPadding={true}>
-          <Craft/>
-          <Activities/>
+          <Craft />
+          <Activities />
         </Container>
-
-        <Token/>
 
         <Container disableVerticalPadding={true}>
-          <Roadmap/>
-          <Team/>
-          <Partners/>
+          <Sequel />
         </Container>
 
-        <Footer/>
+        <Token />
+
+        <Container disableVerticalPadding={true}>
+          <Roadmap />
+          <Team />
+          <Partners />
+        </Container>
+
+        <Footer />
       </div>
     </Layout>
   )
