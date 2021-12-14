851997622de4bae3e49bc376a0f06dab2f1b962a
circuspony <endofrever@gmail.com>
2021-12-14 23:36:21 +0700

    fix: craft

--- src/components/sections/Craft/Board/CraftBoard.tsx
+++ src/components/sections/Craft/Board/CraftBoard.tsx
@@ -7,14 +7,14 @@ interface IProps {
 }
 
 const CraftBoard = (props: IProps) => {
-  const {bigText, smallText} = props;
+  const { bigText, smallText } = props;
 
   return (
     <div className="flex relative">
       <img src="/craft/craftBoard.svg" />
       <div className="flex flex-col items-center justify-center absolute w-full h-full md:h-4/5">
         <Text type="h4" fontWeight="bold">{bigText}</Text>
-        <Text type="h5" className="text-center" opacity={0.4}>{smallText}</Text>
+        <Text type="h5" className="text-center" color={"#80bbc3"}>{smallText}</Text>
       </div>
     </div>
   )
